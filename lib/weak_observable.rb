7d787e480dbb85d4d3a03b06f415ea4379587b3c
Kim Burgestrand <kim@burgestrand.se>
2012-08-27 23:36:19 +0200

    Add implementation of WeakObservable (#add, #delete, #notify)

--- lib/weak_observable.rb
+++ lib/weak_observable.rb
@@ -1,6 +1,78 @@
+require "ref"
 require "weak_observable/version"
-require "weak_observable/weak_set"
 require "weak_observable/mixin"
 
+# WeakObservable is like the Observable from standard library,
+# with a slightly different API and one important difference:
+# it does not keep any strong references to it’s observers.
+#
+# This is useful in case you want observers, but don’t really
+# care if they are garbage collected or not. Mostly useful for
+# asynchronous callbacks in C extensions or FFI bindings.
+#
+# @example
+#   observable = WeakObservable.new
+#   observer   = Object.new
+#
+#   observable.add(observer)
+#   observable.notify # notifies observable
+#
+#   observer = nil
+#   GC.start # this might garbage collect observer
+#   observable.notify # if observer is garbage collected, this does nothing now
 class WeakObservable
+  def initialize
+    @monitor = Ref::SafeMonitor.new
+    @observers = Ref::WeakValueMap.new
+    @observers.extend(Enumerable)
+    @callbacks = Ref::WeakKeyMap.new
+  end
+
+  # Add an observer.
+  #
+  # @param [#hash] observer
+  # @param [String, Symbol] method
+  # @return observer, as passed in.
+  def add(observer, method = :update)
+    synchronize do
+      @callbacks[observer] = method
+      @observers[key(observer)] = observer
+    end
+  end
+
+  # Remove an observer.
+  #
+  # @param [#hash] observer
+  # @return [Object, nil] the observer, or nil if it was not an observer.
+  def delete(observer)
+    synchronize do
+      @callbacks.delete(observer)
+      @observers.delete(key(observer))
+    end
+  end
+
+  # Notify all observers, passing along all arguments and given block.
+  #
+  # @return [Array] all return values.
+  def notify(*args, &block)
+    synchronize do
+      @observers.map do |_, object|
+        callback = @callbacks[object]
+        # rare, but could happen if @callbacks is touched by finalizer
+        # before the @observers
+        next if callback.nil?
+        object.send(callback, *args, &block)
+      end
+    end
+  end
+
+  protected
+
+  def key(observer)
+    observer.hash
+  end
+
+  def synchronize
+    @monitor.synchronize { return yield }
+  end
 end
--- spec/weak_observable/weak_observable_spec.rb
+++ spec/weak_observable/weak_observable_spec.rb
@@ -1,4 +1,7 @@
 describe WeakObservable do
+  let(:observable) { WeakObservable.new }
+  let(:observer) { stub(:update => nil) }
+
   it "has a version" do
     defined?(WeakObservable::VERSION).should eq "constant"
   end
@@ -17,5 +20,75 @@ describe WeakObservable do
         obj.observers.should eql obj.observers
       end
     end
+  end
+
+  describe "#add and #notify" do
+    it "adds an observer" do
+      observer.should_receive(:update)
+
+      observable.add(observer)
+      observable.notify
+    end
+
+    it "allows us to specify callback method" do
+      observer.should_receive(:trigger)
+
+      observable.add(observer, :trigger)
+      observable.notify
+    end
+  end
+
+  describe "#notify" do
+    it "passes along the given block" do
+      observer.should_receive(:update).and_return do |&block|
+        block.call(2)
+      end
+
+      observable.add(observer)
+      observable.notify { |x| x * 2 }.should eq [4]
+    end
+
+    it "returns all return values" do
+      observer1 = stub(:update => 1)
+      observer2 = stub(:update => 2)
+
+      observable.add(observer1)
+      observable.add(observer2)
+
+      observable.notify.should eq [1, 2]
+    end
+  end
+
+  describe "#delete" do
+    it "removes the observer" do
+      observer.should_not_receive(:update)
+
+      observable.add(observer)
+      observable.delete(observer)
+      observable.notify.should eq []
+    end
+  end
+
+  specify "observers can be garbage collected" do
+    counter   = 0
+    finalizer = lambda { |_| counter += 1 }
+
+    threshold = 5
+    stretches = 5
+
+    threshold.times do
+      observer = stub(:update)
+      observable.add(observer)
+      ObjectSpace.define_finalizer(observer, finalizer)
+    end
+
+    (threshold * stretches).times do
+      GC.start
+      sleep 0.001
+    end
+
+    # if one is garbage collected, we can expect all to eventually be
+    # as well
+    counter.should be > 1
   end
 end
